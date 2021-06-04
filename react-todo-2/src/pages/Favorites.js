e5e72cf78f74d108b39c2b7e67ee3ccfa01f8817
nhattan96 <tranhuynhnhattan96@gmail.com>
2021-06-04 11:27:14 +0700

    update

--- react-todo-2/src/pages/Favorites.js
+++ react-todo-2/src/pages/Favorites.js
@@ -1,8 +1,25 @@
-function FavoritesPage () {
+import { useContext } from 'react'
+
+import FavoritesContext from '../store/favories-context'
+import MeetupList from '../components/meetups/MeetupList'
+
+function FavoritesPage() {
+    const favoritesCtx = useContext(FavoritesContext)
+    let content
+
+    if (favoritesCtx.totalFavories === 0) {
+        content = <p>You got no favorites yet. Start adding some?</p>
+    }
+    else {
+        content = <MeetupList meetups={favoritesCtx.favorites} />
+    }
     return (
-        <div>
-            Favorites Page!
-        </div>
+        <section>
+            <h1>
+                Favorites Page!
+            </h1>
+            {content}
+        </section>
     )
 }
 
